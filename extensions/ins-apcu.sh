eb1ad9c1c11346b0ca2a257294e9f296ecd942a7
kleveralt <klever1989@gmail.com>
2018-11-06 01:09:52 +1000

    fix

--- extensions/ins-apcu.sh
+++ extensions/ins-apcu.sh
@@ -6,3 +6,10 @@
     && tar -xf v5.1.12.tar.gz -C apcu --strip-components=1 \
     && ( cd apcu && phpize && ./configure && make -j$(nproc) && make install ) \
     && docker-php-ext-enable apcu
+
+# Install redis extension
+    cd /tmp/extensions \
+    && mkdir redis \
+    && tar -xf redis-${REDIS_VERSION}.tgz -C redis --strip-components=1 \
+    && ( cd redis && phpize && ./configure && make -j$(nproc) && make install ) \
+    && docker-php-ext-enable redis
